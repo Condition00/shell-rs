dadaa9b29f9f4d72a0659c3db13b1b42e632cb57
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-19 11:45:39 +0530

    hmm

---
+++ src/main.rs
@@ -0,0 +1,101 @@
+use pathsearch::find_executable_in_path;
+use std::{
+    io::{self, Write},
+    os::unix::process::CommandExt,
+    process::{self, Command},
+};
+const BUILTINS: &[&str] = &["exit", "echo", "type", "pwd", "cd"];
+
+fn main() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        let input = command.trim();
+
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+
+        let cmd = parts[0];
+        let args = &parts[1..];
+
+        match cmd {
+            "cd" => {
+                if args.is_empty() {
+                    eprintln!("cd: missing opperand");
+                    continue;
+                }
+
+                let dir = args[0];
+
+                //abs paths for now
+
+                if !dir.starts_with("/") {
+                    eprintln!("cd: {}: No such file or directory", dir);
+                    continue;
+                }
+
+                match std::env::set_current_dir(dir) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        eprintln!("cd: {}: No such file or directory", dir);
+                    }
+                }
+            }
+            "exit" => process::exit(0),
+            "echo" => {
+                println!("{}", args.join(" "));
+            }
+            " " => continue,
+            "pwd" => {
+                println!("{}", std::env::current_dir().unwrap().display());
+            }
+
+            "type" => {
+                if args.is_empty() {
+                    println!("type: missing operand");
+                    continue;
+                }
+
+                let target = args[0];
+
+                if BUILTINS.contains(&target) {
+                    println!("{} is a shell builtin", target);
+                } else if let Some(path) = find_executable_in_path(args[0]) {
+                    println!("{} is {}", args[0], path.display());
+                } else {
+                    println!("{}: not found", target);
+                }
+            }
+            _ => {
+                use std::path::PathBuf;
+
+                let path = if cmd.contains("/") {
+                    Some(PathBuf::from(cmd))
+                } else {
+                    find_executable_in_path(cmd)
+                };
+
+                if let Some(path) = path {
+                    let status = Command::new(path).arg0(cmd).args(args).status();
+                    match status {
+                        Ok(status) => {
+                            if !status.success() {
+                                eprintln!("process exited with {}", status);
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("failed to execute {}: {}", cmd, e);
+                        }
+                    }
+                } else {
+                    println!("{}: command not found", cmd);
+                }
+            }
+        }
+    }
+}
